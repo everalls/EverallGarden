6249fc12f6eaf64c7f004910d1f91fdfbe8d1b1d
everall77 <cadair77@gmail.com>
2019-03-18 23:17:15 +0200

    progress

--- EverallGardenFrontEnd/src/app/app.module.ts
+++ EverallGardenFrontEnd/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './background/background.component';
+import { MidgroundComponent } from './midground/midground.component';
+import { ForegroundComponent } from './foreground/foreground.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    MidgroundComponent,
+    ForegroundComponent
   ],
   imports: [
     BrowserModule,
