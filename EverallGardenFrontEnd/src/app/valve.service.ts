6b044093776a70c75874197c1101c4ef9fc591cf
Erika Grin <evgeny.grin@iprosis.com>
2020-11-14 14:27:12 +0200

    Upgrade to Angular 8.2

--- EverallGardenFrontEnd/src/app/valve.service.ts
+++ EverallGardenFrontEnd/src/app/valve.service.ts
@@ -1,7 +1,7 @@
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Valve } from './shared/valve';
 import { Injectable } from '@angular/core';
-import { defineBase } from '@angular/core/src/render3';
+// import { defineBase } from '@angular/core/src/render3';
 
 @Injectable({
   providedIn: 'root'
